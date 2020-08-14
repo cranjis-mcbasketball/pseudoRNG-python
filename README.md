11f6d946fd37c0ddf0efd615f8442941889d0d6f
Mason Borchard <masonborchard@gmail.com>
2020-08-14 06:44:16 -0700

    Created numpy pseudo random number generator script

---
+++ README.md
@@ -0,0 +1,8 @@
+## Heading ##
+  > Pseudo Random Number Generator Python Server
+
+## About ##
+  > This application writes a pseudo random byte to a .txt file every 1000 ms every time a call to the /numbers endpoint is made.
+
+## How to run app after cloning from Github ##
+  > 1. Run server.py
---
+++ numpyPseudoRNG.py
@@ -0,0 +1,14 @@
+import time
+import numpy as np
+
+def getRand():
+  while True:
+      numbers = np.random.randint(0,2,10)
+      numStr = str(numbers)
+      txt_data = open("pseudo_rng_data.txt","a")
+      txt_data.write(numStr + "\n")
+      txt_data.close()
+      print(numbers)
+      return numbers
+      time.sleep(1)
+

be0776ce78e3a7f187602babe25cba0b179ce587
Mason Borchard <masonborchard@gmail.com>
2020-08-14 06:48:41 -0700

    Created python server and  .gitignore

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from numpyPseudoRNG import getRand
+app = Flask(__name__)
+
+
+
+@app.route("/numbers/",)
+def numbers():
+  nums = getRand()
+  print(nums)
+  return str(nums)
+
+numbers()
+@app.route("/",)
+def index():
+  return "welcome to the pseudoRNG python server"
+
+
+if __name__ == "__main__":
+  app.run(host="localhost", port=int("5000"))
